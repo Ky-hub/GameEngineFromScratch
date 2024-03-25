8c59d6ae63a3c4af39e46737e0588fd71ff0dd4f
Ky-hub <1369245532@qq.com>
2024-03-25 17:28:16 +0800

    2024-3-25

---
+++ Framework/Common/BaseApplication.hpp
@@ -0,0 +1,19 @@
+
+#pragma once 
+#include "IApplication.hpp"
+
+namespace My {
+    class BaseApplication : implements IApplication
+    {
+        public:
+            virtual int Initialize();
+            virtual void Finalize();
+            // One cycle of the main loop
+            virtual void Tick();
+
+            virtual bool IsQuit();
+        
+        protected:
+            bool m_bQuit;
+    };
+}
