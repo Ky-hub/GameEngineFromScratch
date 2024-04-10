cfd8a934cfbb00e3004e6037fdf24f7471d1ed35
Ky-hub <1369245532@qq.com>
2024-04-10 15:56:25 +0800

    r_30

--- Framework/GeomMath/geommath.hpp
+++ Framework/GeomMath/geommath.hpp
@@ -1,5 +1,3 @@
-
-
 #pragma once
 #include <cstdint>
 #include <iostream>
@@ -22,8 +20,6 @@
 #define TWO_PI 3.14159265358979323846f * 2.0f
 #endif
 
-
-
 namespace My {
     template<typename T, size_t SizeOfArray>
         constexpr size_t countof(T (&)[SizeOfArray]) { return SizeOfArray; }
@@ -31,11 +27,18 @@ namespace My {
     template<typename T, size_t RowSize, size_t ColSize>
         constexpr size_t countof(T (&)[RowSize][ColSize]) { return RowSize * ColSize; }
 
+#ifdef max
+    #undef max
+#endif
+#ifdef min
+    #undef min
+#endif
+
     template<typename T>
         constexpr float normalize(T value) {
             return value < 0
-                ? -static_cast<float>(value) / (std::numeric_limits<T>::min)()
-                :  static_cast<float>(value) / (std::numeric_limits<T>::max)()
+                ? -static_cast<float>(value) / std::numeric_limits<T>::min()
+                :  static_cast<float>(value) / std::numeric_limits<T>::max()
                 ;
         }
 
@@ -137,9 +140,16 @@ namespace My {
 
         operator T*() { return data; };
         operator const T*() const { return static_cast<const T*>(data); };
+        Vector4Type& operator=(const T* f) 
+        { 
+            memcpy(data, f, sizeof(T) * 4); 
+            return *this;
+        };
+        
     };
 
     typedef Vector4Type<float> Vector4f;
+    typedef Vector4Type<float> Quaternion;
     typedef Vector4Type<uint8_t> R8G8B8A8Unorm;
     typedef Vector4Type<uint8_t> Vector4i;
 
@@ -223,6 +233,12 @@ namespace My {
 
         operator T*() { return &data[0][0]; };
         operator const T*() const { return static_cast<const T*>(&data[0][0]); };
+
+        Matrix& operator=(const T* _data) 
+        {
+            memcpy(data, _data, ROWS * COLS * sizeof(T));
+            return *this;
+        }
     };
 
     typedef Matrix<float, 4, 4> Matrix4X4f;
@@ -438,6 +454,20 @@ namespace My {
         return;
     }
 
+    inline void MatrixScale(Matrix4X4f& matrix, const float x, const float y, const float z)
+    {
+        Matrix4X4f scale = {{{
+            {    x, 0.0f, 0.0f, 0.0f},
+            { 0.0f,    y, 0.0f, 0.0f},
+            { 0.0f, 0.0f,    z, 0.0f},
+            { 0.0f, 0.0f, 0.0f, 1.0f},
+        }}};
+
+        matrix = scale;
+
+        return;
+    }
+
     inline void MatrixRotationY(Matrix4X4f& matrix, const float angle)
     {
         float c = cosf(angle), s = sinf(angle);
@@ -471,4 +501,29 @@ namespace My {
         return;
     }
 
+    inline void MatrixRotationAxis(Matrix4X4f& matrix, const Vector3f& axis, const float angle)
+    {
+        float c = cosf(angle), s = sinf(angle), one_minus_c = 1.0f - c;
+
+        Matrix4X4f rotation = {{{
+            {   c + axis.x * axis.x * one_minus_c,  axis.x * axis.y * one_minus_c + axis.z * s, axis.x * axis.z * one_minus_c - axis.y * s, 0.0f    },
+            {   axis.x * axis.y * one_minus_c - axis.z * s, c + axis.y * axis.y * one_minus_c,  axis.y * axis.z * one_minus_c + axis.x * s, 0.0f    },
+            {   axis.x * axis.z * one_minus_c + axis.y * s, axis.y * axis.z * one_minus_c - axis.x * s, c + axis.z * axis.z * one_minus_c, 0.0f },
+            {   0.0f,  0.0f,  0.0f,  1.0f   }
+        }}};
+
+        matrix = rotation;
+    }
+
+    inline void MatrixRotationQuaternion(Matrix4X4f& matrix, Quaternion q)
+    {
+        Matrix4X4f rotation = {{{
+            {   1.0f - 2.0f * q.y * q.y - 2.0f * q.z * q.z,  2.0f * q.x * q.y + 2.0f * q.w * q.z,   2.0f * q.x * q.z - 2.0f * q.w * q.y,    0.0f    },
+            {   2.0f * q.x * q.y - 2.0f * q.w * q.z,    1.0f - 2.0f * q.x * q.x - 2.0f * q.z * q.z, 2.0f * q.y * q.z + 2.0f * q.w * q.x,    0.0f    },
+            {   2.0f * q.x * q.z + 2.0f * q.w * q.y,    2.0f * q.y * q.z - 2.0f * q.y * q.z - 2.0f * q.w * q.x, 1.0f - 2.0f * q.x * q.x - 2.0f * q.y * q.y, 0.0f    },
+            {   0.0f,   0.0f,   0.0f,   1.0f    }
+        }}};
+
+        matrix = rotation;
+    }
 }
